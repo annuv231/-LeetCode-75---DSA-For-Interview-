2de79d981a12027d5b0ff2dd27614da1c4adacdc
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-13 22:47:46 +0530

    Time: 2 ms (63.47%), Space: 7.1 MB (71.94%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,16 +11,13 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp=head;
-        int count=0;
-        while(temp!=NULL){
-count++; temp=temp->next;}
-        cout<<count<<endl;
-             count=count/2;
-             while(count--){
-                 head=head->next;
-             }
-             return head;
-
+        //another method using rabbit and hair method
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+               slow=slow->next;
+                fast=fast->next->next;
+        }
+       return slow;
     }
 };
