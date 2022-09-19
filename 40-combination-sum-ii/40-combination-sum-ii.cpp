6cbb7a85bd9f98312976d1fda19bdac11b8b6f70
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-19 22:38:33 +0530

    Time: 6 ms (80.13%), Space: 10.5 MB (89.40%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    public:
+    
+      void subesequence(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int idx,int target,int n){
+      if(target==0){
+          ans.push_back(temp);
+              return;
+         }
+        
+        //using striver method
+       for(int i=idx;i<arr.size();i++){
+           if(i>idx && arr[i]==arr[i-1]){
+               continue;
+           }
+           if(arr[i]>target){
+               break;
+           }
+           temp.push_back(arr[i]);
+           subesequence(arr,ans,temp,i+1,target-arr[i],n);
+           temp.pop_back();
+           
+       }
+      }
+    
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(candidates.begin(),candidates.end());
+        subesequence(candidates,ans,temp,0,target,n);
+        return ans;
+    }
+};
