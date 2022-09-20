6421c5ec267aac5efc003b9bb8b8cf409b7a9d1e
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-20 23:35:25 +0530

    Time: 3 ms (83.20%), Space: 7.7 MB (55.24%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,18 +1,31 @@
 class Solution {
+    //Method 2:
+// public:
+//     void ansPermute(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,vector<int>track,int idx ,int n){
+//             if(temp.size()==n){
+//                 ans.push_back(temp);
+//             return;
+//             }
+//         for(int i=0;i<n;i++){
+//             if(!track[i]){
+//             temp.push_back(arr[i]);
+//             track[i]=1;
+//             ansPermute(arr,ans,temp,track,i+1,n);
+//             track[i]=0;
+//             temp.pop_back();
+//             }
+//         }
+//     }
 public:
-    void ansPermute(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,vector<int>track,int idx ,int n){
-            if(temp.size()==n){
-                ans.push_back(temp);
+    void ansPermute(vector<int>&arr,vector<vector<int>>&ans,int idx ){
+        if(idx==arr.size()){
+            ans.push_back(arr);
             return;
-            }
-        for(int i=0;i<n;i++){
-            if(!track[i]){
-            temp.push_back(arr[i]);
-            track[i]=1;
-            ansPermute(arr,ans,temp,track,i+1,n);
-            track[i]=0;
-            temp.pop_back();
-            }
+        }
+        for(int i=idx;i<arr.size();i++){
+            swap(arr[i],arr[idx]);
+            ansPermute(arr,ans,idx+1);
+            swap(arr[i],arr[idx]);
         }
     }
 public:
@@ -21,7 +34,7 @@ public:
         vector<int>temp;
         int n=nums.size();
         vector<int>track(n,0);
-        ansPermute(nums,ans,temp,track,0,n);
+        ansPermute(nums,ans,0);
         
         return ans;
     }
