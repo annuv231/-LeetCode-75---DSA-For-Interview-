62eb5970a4cfe97ed1ac795ed94d20e505b71ded
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-20 22:11:06 +0530

    Time: 5 ms (45.95%), Space: 7.1 MB (71.30%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,17 +1,32 @@
 class Solution {
-    public:
-     void ansSub(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int idx,int n){
-    if(idx==n){
-      ans.push_back(temp);
-        return;
-    }
     
-    temp.push_back(arr[idx]);
-    ansSub(arr,ans,temp,idx+1,n);
-    temp.pop_back();
-    ansSub(arr,ans,temp,idx+1,n);
-         return;
-     }
+    //Method 1;
+//     public:
+//      void ansSub(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int idx,int n){
+//     if(idx==n){
+//       ans.push_back(temp);
+//         return;
+//     }
+    
+//     temp.push_back(arr[idx]);
+//     ansSub(arr,ans,temp,idx+1,n);
+//     temp.pop_back();
+//     ansSub(arr,ans,temp,idx+1,n);
+//          return;
+//      }
+   public:
+    void ansSub(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int idx,int n){
+        ans.push_back(temp);
+        for(int i=idx;i<n;i++){
+            if(i>idx && arr[i]==arr[i-1]){
+                continue;
+            }
+            temp.push_back(arr[i]);
+            ansSub(arr,ans,temp,i+1,n);
+            temp.pop_back();
+        }
+        return;
+    } 
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         sort(nums.begin(),nums.end());
