7cca37b79d3a06fcff3df4f876c792facde5ae7d
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-20 22:11:17 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (71.30%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -18,9 +18,7 @@ class Solution {
     void ansSub(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int idx,int n){
         ans.push_back(temp);
         for(int i=idx;i<n;i++){
-            if(i>idx && arr[i]==arr[i-1]){
-                continue;
-            }
+            
             temp.push_back(arr[i]);
             ansSub(arr,ans,temp,i+1,n);
             temp.pop_back();
