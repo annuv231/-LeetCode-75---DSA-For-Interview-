0d37d618c61bdd9f9d6b87dbd1403dbbd9eb16f2
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-22 15:14:17 +0530

    Time: 21 ms (62.37%), Space: 9.7 MB (45.42%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -3,19 +3,31 @@ public:
     string reverseWords(string s) {
         // reverse(s.begin(),s.end());
         // return s;
-        string ans="";
-        string temp="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]==' '){
-                reverse(temp.begin(),temp.end());
-                ans+=temp;
-                ans+=' ';
-                temp="";
+        // string ans="";
+        // string temp="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]==' '){
+        //         reverse(temp.begin(),temp.end());
+        //         ans+=temp;
+        //         ans+=' ';
+        //         temp="";
+        //     }
+        //     else temp=temp+s[i];
+        // }
+        // reverse(temp.begin(),temp.end());
+        // ans+=temp;
+        
+        
+        //method 2: inplace:
+        
+        int i=0;
+        for(int j=0;j<s.length();j++){
+            if(s[j]==' '){
+              reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
             }
-            else temp=temp+s[i];
         }
-        reverse(temp.begin(),temp.end());
-        ans+=temp;
-        return ans;
+        reverse(s.begin()+i,s.end());
+        return s;
     }
 };
