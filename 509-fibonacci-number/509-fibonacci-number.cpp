c43f70c25befe9535e8e1f5102f5233bcda0c98e
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-26 22:41:32 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.58%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -9,22 +9,43 @@
 //         return fib(n-1)+fib(n-2);
 //     }
 // };
+
+//method 2 -x-x-x-x-x-xxxxxxx-xx-x-x-x
+// class Solution {
+    
+// public:
+//     int dpFib(int n, vector<int>&dp){
+//         if(n==0||n==1){
+//             dp[n]=n;
+//             return n;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         else return dp[n]=dpFib(n-1,dp)+dpFib(n-2,dp);
+//     }
+//     int fib(int n) {
+//         vector<int>dp(n+1,-1);
+//         dpFib(n,dp);
+//         return dp[n];
+//     }
+// };
+
 class Solution {
     
 public:
-    int dpFib(int n, vector<int>&dp){
-        if(n==0||n==1){
-            dp[n]=n;
+    int fib(int n) {
+        if(n<2){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        else return dp[n]=dpFib(n-1,dp)+dpFib(n-2,dp);
-    }
-    int fib(int n) {
-        vector<int>dp(n+1,-1);
-        dpFib(n,dp);
-        return dp[n];
+        int dp[n+1];
+         dp[0]=0;
+         dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }  
+        
+        return dp[n];;
+        
     }
 };
