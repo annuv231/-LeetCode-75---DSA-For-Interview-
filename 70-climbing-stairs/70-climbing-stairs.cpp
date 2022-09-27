ff47f95e4fa20cb73284957b6f6e7f308bab99e3
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-27 17:20:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (31.84%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        solve(dp,n);
-        return dp[n];
-    }
-    
-    int solve(vector<int>&dp,int n){
-        if(n==0||n==1){
-          dp[n]=1;
-         return 1;
+    int ansStairs(vector<int>&dp,int n){
+          if(n==0||n==1){
+            dp[n]=1;
+            return dp[n];
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        return dp[n]=ansStairs(dp,n-1)+ansStairs(dp,n-2);
     }
-    
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+         ansStairs(dp,n);
+        return dp[n];
+       
+    }
 };
