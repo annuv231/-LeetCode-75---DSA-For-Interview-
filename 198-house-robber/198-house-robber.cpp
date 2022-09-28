3c66533aada55f5ec920a3c1dfbd0f2feed1af58
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-28 19:36:05 +0530

    Time: 5 ms (23.68%), Space: 7.9 MB (26.10%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -32,11 +32,30 @@ public:
         }
         return dp[n-1];
     }
+    //space bottomup optimization
+    int ansRobBottomSpace(vector<int>&nums,int n){
+        // int dp[n];
+        int p1=nums[0];
+        int p2=0;
+        int curr=p1;
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i-2>=0){
+                pick+=p2;
+            }
+            int notPick=p1;
+            curr=max(pick,notPick);
+            p2=p1;
+            p1=curr;
+            
+        }
+        return curr;
+    }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return ansRobBottom(nums,n);
+        return ansRobBottomSpace(nums,n);
         // return ansRob(nums,n-1,dp);
     }
 };
