c6365273f4681ac113fe860cd2a8a61a4b3b0069
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-09-28 19:32:46 +0530

    Time: 8 ms (8.15%), Space: 7.9 MB (26.10%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,6 +1,8 @@
 class Solution {
+    
+    //top down
 public:
-    int ansRob(vector<int>&nums,int n,vector<int>&dp){
+    int ansRobTop(vector<int>&nums,int n,vector<int>&dp){
         if(n==0){
             return dp[n]=nums[n];
         }
@@ -10,14 +12,31 @@ public:
         if(dp[n]!=-1){
             return dp[n];
         }
-        int pick=nums[n]+ansRob(nums,n-2,dp);
-        int notPick=0+ansRob(nums,n-1,dp);
+        int pick=nums[n]+ansRobTop(nums,n-2,dp);
+        int notPick=0+ansRobTop(nums,n-1,dp);
         return dp[n]=max(pick,notPick);
+    }
+    
+//bottom up
+    int ansRobBottom(vector<int>&nums,int n){
+        int dp[n];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i-2>=0){
+                pick+=dp[i-2];
+            }
+            int notPick=dp[i-1];
+            dp[i]=max(pick,notPick);
+            
+        }
+        return dp[n-1];
     }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return ansRob(nums,n-1,dp);
+        return ansRobBottom(nums,n);
+        // return ansRob(nums,n-1,dp);
     }
 };
