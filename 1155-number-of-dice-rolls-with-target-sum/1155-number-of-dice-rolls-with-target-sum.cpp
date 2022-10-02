0ede452073647fda4f31a07cc8cb6c873d39087a
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-10-02 21:47:36 +0530

    Time: 69 ms (32.89%), Space: 8.4 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public: 
     int MOD=1e9+7;
+    int temp=0;
     int ansSum(int n,int k,int target,vector<vector<int>>&dp){
-        if(n==0){
-            if(target==0){
-                return dp[n][target]=1;
-            }
-            else return 0;
+        if(n==0&&target==0){
+            return 1;
         }
-        else if(n<0){
+        if((n==0&&target!=0)||(n!=0&&target==0)){
+            return 0;
+        }
+        if(n<0||target<0){
             return 0;
         }
         if(dp[n][target]!=-1){
@@ -16,10 +17,10 @@ public:
         }
         int ans=0;
         for(int i=1;i<=k;i++){
-            if(i<=target){
+               if(target-i>=0){
                 ans=(ans%MOD+ansSum(n-1,k,target-i,dp)%MOD)%MOD;
+               }
                 // cout<<"ans "<<ans<<endl;
-            }
         }
         return dp[n][target]=ans;
     }
@@ -27,22 +28,5 @@ public:
     int numRollsToTarget(int n, int k, int target) {
         vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
         return ansSum(n,k,target,dp);
-//             int MOD=1e9+7;
-//         //bottom up approach
-//         dp[0][0]=1;
-//         for(int i=1;i<=n;i++){
-//             for(int j=1;j<=target;j++){
-//                 int ans=0;
-//                 for(int f=1;f<=k;f++){
-//                        if(j-f>=0){
-//                           ans=(ans+dp[i-1][j-f])%MOD; 
-//                        }
-                       
-//                         // cout<<"ans "<<ans<<endl;
-//                 }
-//                 dp[i][j]=ans;
-//             }
-//         }
-//         return dp[n][target];
     }
 };
