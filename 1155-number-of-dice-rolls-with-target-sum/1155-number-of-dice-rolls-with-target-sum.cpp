9b5c4ff2853795d6360604fb48a3ef99ec31d661
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-10-02 21:47:51 +0530

    Time: 85 ms (25.75%), Space: 8.5 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -17,9 +17,7 @@ public:
         }
         int ans=0;
         for(int i=1;i<=k;i++){
-               if(target-i>=0){
                 ans=(ans%MOD+ansSum(n-1,k,target-i,dp)%MOD)%MOD;
-               }
                 // cout<<"ans "<<ans<<endl;
         }
         return dp[n][target]=ans;
