8cae47285d734656956b4d77fed87743092a6d05
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-10-10 18:50:05 +0530

    Time: 4 ms (29.56%), Space: 6 MB (99.82%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
-        int low=0;
-        int last=s.length()-1;
-        while(low<=last){
-            if(s[low]!=s[last]){
-                return false;
-            }
-            low++;
-            last--;
-        }
-        return true;
-    }
+    // bool isPalindrome(string s){
+    //     int low=0;
+    //     int last=s.length()-1;
+    //     while(low<=last){
+    //         if(s[low]!=s[last]){
+    //             return false;
+    //         }
+    //         low++;
+    //         last--;
+    //     }
+    //     return true;
+    // }
     string breakPalindrome(string palindrome) {
          string ans="";
         int n=palindrome.length();
-        if(n==1){
+        if(n<=1){
             return ans;
         }
         
         //brute force approach
-        for(int i=0;i<n;i++){
-            ans+='z';
-        }
-        for(char i='a';i<='b';i++){
+//         for(int i=0;i<n;i++){
+//             ans+='z';
+//         }
+//         for(char i='a';i<='b';i++){
                
-            for(int j=0;j<n;j++){
-                string temp=palindrome;
-                temp[j]=i;
-                if(!isPalindrome(temp)){
-                    ans=min(temp,ans);
-                }
+//             for(int j=0;j<n;j++){
+//                 string temp=palindrome;
+//                 temp[j]=i;
+//                 if(!isPalindrome(temp)){
+//                     ans=min(temp,ans);
+//                 }
+//             }
+//         }
+        
+        //greedy
+        
+        for(int i=0;i<n/2;i++){
+            if(palindrome[i]!='a'){
+                palindrome[i]='a';
+                return palindrome;
             }
+            
         }
-        return ans;
+        palindrome[n-1]='b';
+        return palindrome;
     }
 };
