cee79bf6f5ebf0de76a15272ebd1941d4dfccffa
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2022-10-19 21:09:18 +0530

    Time: 41 ms (6.86%), Space: 12.8 MB (18.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,29 +1,22 @@
 class Solution {
-
 public:
- bool static sortbysecdesc(const pair<string,int>a,
-                   const pair<string,int>b)
-{      if(a.second==b.second){
-    return a.first<b.first;
-}
-       return a.second>b.second;
-}
- 
-vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int>mp;
-        for(auto x: words){
-            mp[x]++;
-        }
-        vector<pair<string,int>>arr;
-        for(auto& x: mp){
-           arr.push_back(x);
-        }
-       sort(arr.begin(), arr.end(), sortbysecdesc);
-       vector<string>ans;
-        for (int i=0; i<k; i++)
-    {
-        ans.push_back(arr[i].first);
+  vector<string> topKFrequent(vector<string>& a, int k) {
+    map<string,int> mp;
+    for (auto x : a) mp[x]++;
+    vector<pair<int,string>> b;
+    for (auto &[k,v] : mp) {
+      b.push_back({-v,k});
+        cout<<k<<": "<<v<<endl;
     }
-        return ans;
+    sort(b.begin(), b.end());
+     for(auto x:b){
+         cout<<x.first<<": "<<x.second<<endl;
+     }
+    vector<string> ans;
+    for (int i=0; i<k; i++) {
+      auto [cnt,x] = b[i];
+      ans.push_back(x);
     }
+    return ans;
+  }
 };
